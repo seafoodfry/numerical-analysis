8bc58fee033bacee0ef961ee3e3cdc839d1c737d
seafoodfry <>
2024-04-05 23:39:23 -0400

    Working example
    Signed-off-by: seafoodfry <>

--- qft/random-walks/Basic2D_pythonic.pyx
+++ qft/random-walks/Basic2D_pythonic.pyx
@@ -3,7 +3,7 @@
 import numpy as np
 
 cpdef run_random_walks_pythonic():
-    cdef int sampleSize = 100000
+    cdef int sampleSize = 100
     cdef int minLength = 1
     cdef int maxLength = 50
     cdef int x, y
@@ -11,13 +11,13 @@ cpdef run_random_walks_pythonic():
     cdef int totalSteps, test, step
     cdef double dist, distTotal, distAvg
 
-    # Declare directions as a NumPy array without Cython static typing
-    # Adjust dtype to np.intp for alignment with C array indexing
     for totalSteps in range(minLength, maxLength + 1):
         distTotal = 0
         for test in range(sampleSize):
-            x = y = 0
-            directions = np.random.randint(0, 4, size=totalSteps, dtype=np.int64)
+            x = y = 0        
+            directions = np.random.randint(0, 4, size=totalSteps)
+
+            # Do the entire random walk.
             for step in range(totalSteps):
                 # Directly use directions with Pythonic indexing
                 direction = directions[step]
@@ -25,9 +25,11 @@ cpdef run_random_walks_pythonic():
                 elif direction == 1: y += stepLength
                 elif direction == 2: x -= stepLength
                 else: y -= stepLength
+
+            # Now that the walk is over, let's see how far we got.
             dist = x * x + y * y
             distTotal += dist
 
         distAvg = distTotal / sampleSize
-        with open("Basic2D_pythonic.csv", "w") as output:
+        with open("Basic2D_pythonic.csv", "a") as output:
             output.write(f"{totalSteps},\t{distAvg:.3f}\n")
