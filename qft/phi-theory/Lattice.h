22191e8c61340badee9856e0bd1fb13a7b30f23e
seafoodfry <>
2024-04-14 16:51:53 -0400

    Init commit for phi^4 theory
    Signed-off-by: seafoodfry <>

---
+++ qft/phi-theory/Lattice.h
@@ -0,0 +1,12 @@
+/* Lattice.h
+Implements a lattice of spins for a phi^4 simulation using a \mu action.
+
+This is based on https://inspirehep.net/literature/1386200 ,
+Lattice Simulations of Nonperturbative Quantum Field Theories
+by David Schaich
+*/
+#ifndef _LATTICE_H
+#define _LATTICE_H
+
+
+#endif // _LATTICE_H
